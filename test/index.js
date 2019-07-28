9febdeca29a0f182cba6f2b1af7fbe14c23d70c5
umut biçer <seyitumutbicer@gmail.com>
2019-07-28 21:04:27 +0300

    dev

--- test/index.js
+++ test/index.js
@@ -1,2 +1,13 @@
+//import { mdb } from '../dist/index.js'
+var mdb=require('../dist/index.js').default
+console.log('a')
 
+var db=mdb()
+db.add('d',{sayi:5})
+db.add('a',{sayi:51})
+db.add('s',{sayi:4})
+db.add('v',{sayi:3})
+db.add('h',{sayi:5})
+
+console.log(db.db)
 
